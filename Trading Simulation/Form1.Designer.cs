824dfdba3ad1996de381d63cbfaa67d7172a941e
kamal chanchal <kchanchal78@gmail.com>
2024-01-16 17:14:36 +0530

    #1  Trades Execution

--- Trading Simulation/Form1.Designer.cs
+++ Trading Simulation/Form1.Designer.cs
@@ -28,19 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgv_trades = new System.Windows.Forms.DataGridView();
+            this.button3_sell = new System.Windows.Forms.Button();
+            this.button2_buy = new System.Windows.Forms.Button();
+            this.button1_starttradong = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1_ltp = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_trades)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1584, 947);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.textBox1_ltp);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dgv_trades);
+            this.panel1.Controls.Add(this.button3_sell);
+            this.panel1.Controls.Add(this.button2_buy);
+            this.panel1.Controls.Add(this.button1_starttradong);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(15, 330);
+            this.panel1.Margin = new System.Windows.Forms.Padding(15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1554, 285);
+            this.panel1.TabIndex = 0;
+            // 
+            // dgv_trades
+            // 
+            this.dgv_trades.AllowUserToAddRows = false;
+            this.dgv_trades.AllowUserToDeleteRows = false;
+            this.dgv_trades.AllowUserToResizeColumns = false;
+            this.dgv_trades.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_trades.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_trades.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgv_trades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_trades.Location = new System.Drawing.Point(11, 93);
+            this.dgv_trades.Name = "dgv_trades";
+            this.dgv_trades.ReadOnly = true;
+            this.dgv_trades.RowHeadersVisible = false;
+            this.dgv_trades.RowHeadersWidth = 62;
+            this.dgv_trades.RowTemplate.Height = 30;
+            this.dgv_trades.Size = new System.Drawing.Size(1532, 184);
+            this.dgv_trades.TabIndex = 3;
+            // 
+            // button3_sell
+            // 
+            this.button3_sell.Location = new System.Drawing.Point(1276, 14);
+            this.button3_sell.Name = "button3_sell";
+            this.button3_sell.Size = new System.Drawing.Size(213, 47);
+            this.button3_sell.TabIndex = 2;
+            this.button3_sell.Text = "Sell";
+            this.button3_sell.UseVisualStyleBackColor = true;
+            this.button3_sell.Click += new System.EventHandler(this.button3_sell_Click);
+            // 
+            // button2_buy
+            // 
+            this.button2_buy.Location = new System.Drawing.Point(950, 14);
+            this.button2_buy.Name = "button2_buy";
+            this.button2_buy.Size = new System.Drawing.Size(213, 47);
+            this.button2_buy.TabIndex = 1;
+            this.button2_buy.Text = "Buy";
+            this.button2_buy.UseVisualStyleBackColor = true;
+            this.button2_buy.Click += new System.EventHandler(this.button2_buy_Click);
+            // 
+            // button1_starttradong
+            // 
+            this.button1_starttradong.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1_starttradong.Location = new System.Drawing.Point(19, 14);
+            this.button1_starttradong.Name = "button1_starttradong";
+            this.button1_starttradong.Size = new System.Drawing.Size(213, 47);
+            this.button1_starttradong.TabIndex = 0;
+            this.button1_starttradong.Text = "Start Trading";
+            this.button1_starttradong.UseVisualStyleBackColor = true;
+            this.button1_starttradong.Click += new System.EventHandler(this.button1_starttradong_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(346, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tick";
+            // 
+            // textBox1_ltp
+            // 
+            this.textBox1_ltp.Location = new System.Drawing.Point(415, 12);
+            this.textBox1_ltp.Name = "textBox1_ltp";
+            this.textBox1_ltp.ReadOnly = true;
+            this.textBox1_ltp.Size = new System.Drawing.Size(184, 31);
+            this.textBox1_ltp.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 615);
+            this.ClientSize = new System.Drawing.Size(1584, 947);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_trades)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private DataGridView dgv_trades;
+        private Button button3_sell;
+        private Button button2_buy;
+        private Button button1_starttradong;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private TextBox textBox1_ltp;
+        private Label label1;
     }
 }
--- Trading Simulation/Form1.cs
+++ Trading Simulation/Form1.cs
@@ -1,10 +1,212 @@
+using System.Data;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+
 namespace Trading_Simulation
 {
     public partial class Form1 : Form
     {
+        #region  : SECTION DEFINE VARIABLES
+
+        private bool mFlag_IStradingStarted= false;
+
+
+        private DataTable Dt_trades_Execution;
+        Random random;
+
+
+        public static System.Windows.Forms.Timer Timer_feed;
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+
+            this.Dt_trades_Execution = new DataTable();
+            random = new Random();
+
+
+
+            // initialise all Functions here..
+            Design_Trades_execution();
+            Set_timer();
+        }
+
+        private void Set_timer()
+        {
+            Timer_feed = new System.Windows.Forms.Timer();
+            Timer_feed.Interval = 12;
+            Timer_feed.Tick += new System.EventHandler(this.Event_update_tick); //Method attached
+        }
+
+
+        private void Design_Trades_execution()
+        {
+            // add Trading Columns  In Data Table
+
+            // Add columns to the DataTable
+            Dt_trades_Execution.Columns.Add("Symbol", typeof(string));
+            Dt_trades_Execution.Columns.Add("Quantity", typeof(int));
+            Dt_trades_Execution.Columns.Add("Price", typeof(double));
+            Dt_trades_Execution.Columns.Add("OrderType", typeof(string));
+            Dt_trades_Execution.Columns.Add("EntryTime", typeof(DateTime));
+            Dt_trades_Execution.Columns.Add("Status", typeof(string));
+            Dt_trades_Execution.Columns.Add("Ltp", typeof(double));
+            Dt_trades_Execution.Columns.Add("ExitPrice", typeof(double));
+            Dt_trades_Execution.Columns.Add("ExitTime", typeof(string));
+
+
+
+            // bind with view
+            dgv_trades.DataSource = Dt_trades_Execution;
+
+        }
+
+        private void Event_update_tick (object sender, EventArgs e)
+        {
+            get_live_tick();
+        }
+
+
+        private double get_live_tick()
+        {
+            double ltp = 0.0;
+            try
+            {
+
+                ltp = Math.Round(random.NextDouble() * (21670 - 21650) + 21650, 2);
+                //update_LTP_in_gui();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to get LTP - Tick : {ex.Message}");
+            }
+
+            return ltp;
+
+        }
+
+        private void update_LTP_in_gui()
+        {
+            try
+            {
+                double price = get_live_tick();
+
+                if(price != 0.0)
+                {
+                    this.textBox1_ltp.Text = price.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to update lrp in View : {ex.Message}");
+            }
+        }
+
+
+        private void update_trades_pnl()
+        {
+
+        }
+
+
+
+        private void Generate_new_trades(ref DataTable dt , string ordertype)
+        {
+
+            try
+            {
+
+                // Create a new DataRow
+                DataRow newRow = dt.NewRow();
+
+
+                string symbol = "NIFTY 50";
+                Int64 quantity = 1;
+
+                //Random Number as a Stocks Entry Price
+                Random random = new Random();
+
+                // Generating a random number between 21650 and 21670 (inclusive)
+                double price = Math.Round( random.NextDouble() * (21670 - 21650) + 21650 , 2);
+
+
+                // Set values for each column
+                newRow["Symbol"] = symbol;
+                newRow["Quantity"] = quantity;
+                newRow["Price"] = price;
+                newRow["OrderType"] = ordertype;
+                newRow["EntryTime"] = DateTime.Now;
+                newRow["Status"] = Status.mOpen;
+                newRow["Ltp"] = price;
+                newRow["ExitPrice"] = -1;
+                newRow["ExitTime"] = -1;
+
+
+                // Add the DataRow to the DataTable
+                dt.Rows.Add(newRow);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to Load Delegate : {ex.Message}");
+            }
+        }
+
+        private void button1_starttradong_Click(object sender, EventArgs e)
+        {
+            if (mFlag_IStradingStarted == false)
+            {
+                //Start Trading mFlag_tradingStarted = True
+                mFlag_IStradingStarted = true;
+
+                Timer_feed.Start();
+            }
+
+        }
+
+        private void button2_buy_Click(object sender, EventArgs e)
+        {
+            if(mFlag_IStradingStarted== true)
+            {
+                Generate_new_trades(ref this.Dt_trades_Execution, OrderType.mBUY);
+            }
+            else
+            {
+                MessageBox.Show("Please Start Trading... Than Proceed");
+            }
+
+
+        }
+
+        private void button3_sell_Click(object sender, EventArgs e)
+        {
+            if (mFlag_IStradingStarted == true)
+            {
+                Generate_new_trades(ref this.Dt_trades_Execution, OrderType.mSELL);
+            }
+            else
+            {
+                MessageBox.Show("Please Start Trading... Than Proceed");
+            }
         }
     }
+
+    public class Status
+    {
+        public const string  mOpen  = "Open";
+        public const string mClose  = "Close";
+
+    }
+
+    public class OrderType
+    {
+        public const string mBUY = "B";
+        public const string mSELL = "S";
+    }
+
+    
 }
